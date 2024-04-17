f91d1dd2a56ac145772165bd96877af5786645b3
sumit <sumitashwanithakur2002@gmail.com>
2024-04-17 22:25:22 +0530

    defining routes

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>s9gnup</div>;
+}
+
+export default page;
---
+++ app/(root)/ask-a-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>askAQuestion</div>;
+}
+
+export default page;
